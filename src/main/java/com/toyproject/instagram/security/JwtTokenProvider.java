cdbd136bb60b9c4fdef0812762f81c8e24b313cb
fjfj10 <kimmisuk2218@gmail.com>
2023-10-04 17:37:56 +0900

    feat: 로그인 후 토큰에 들어가는 username을 로그인 정보가 아닌 로그인 정보의 username을 가지고 오도록 변경

--- src/main/java/com/toyproject/instagram/security/JwtTokenProvider.java
+++ src/main/java/com/toyproject/instagram/security/JwtTokenProvider.java
@@ -1,10 +1,17 @@
 package com.toyproject.instagram.security;
 
+import com.toyproject.instagram.entity.User;
+import com.toyproject.instagram.repository.UserMapper;
+import com.toyproject.instagram.service.PrincipalDetailsService;
+import com.toyproject.instagram.service.UserService;
+import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -12,18 +19,24 @@ import org.springframework.util.StringUtils;
 import java.security.Key;
 import java.util.Date;
 
-// JWT 토큰을 생성 해주는 로직
+// JWT 토큰을 관리해주는 로직
 @Component
 public class JwtTokenProvider {
 
     private final Key key;
+    private final PrincipalDetailsService principalDetailsService;
+    private final UserMapper userMapper;
 
     // Autowired는 IoC 컨테이너에서 객체를 자동 주입
     // Value는 application.yml에서 변수 데이터 자동 주입
 
     // IoC에서 생성 시 yml에서 jwt.secret을 가지고 온다
-    public JwtTokenProvider(@Value("${jwt.secret}")String secret) {
+    public JwtTokenProvider(@Value("${jwt.secret}")String secret,
+                            @Autowired PrincipalDetailsService principalDetailsService,
+                            @Autowired UserMapper userMapper) {
         key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+        this.principalDetailsService = principalDetailsService;
+        this.userMapper = userMapper;
     }
 
     // JWT 토큰을 생성하는 로직
@@ -38,14 +51,24 @@ public class JwtTokenProvider {
         // (1000 * 60 * 60 * 24) 24시간동안 유효한 Token
         Date tokenExpiresDate = new Date(new Date().getTime() + (1000 * 60 * 60 * 24));
 
-        accessToken = Jwts.builder()
+        JwtBuilder jwtBuilder = Jwts.builder()
                 .setSubject("AccessToken")
                 .claim("username", principalUser.getUsername())
                 .setExpiration(tokenExpiresDate)
-                .signWith(key, SignatureAlgorithm.HS256)
-                .compact();
+                .signWith(key, SignatureAlgorithm.HS256);
 
-        return accessToken;
+        User user = userMapper.findUserByPhone(principalUser.getUsername());
+
+        if(user != null) {
+            return jwtBuilder.claim("username", user.getUsername()).compact();
+        }
+
+        user = userMapper.findUserByEmail(principalUser.getUsername());
+        if(user != null) {
+            return jwtBuilder.claim("username", user.getUsername()).compact();
+        }
+
+        return jwtBuilder.claim("username", user.getUsername()).compact();
     }
 
     // 토큰 유효성 검사
@@ -68,6 +91,24 @@ public class JwtTokenProvider {
             return bearerToken.substring(type.length());
         }
         return "";
+    }
+
+    public Authentication getAuthentication(String accessToken) {
+        Authentication authentication = null;
+        // 암호화 해제 후 body를 가지고와 username을 키값으로 가진 값을 가지고온다
+        String username = Jwts
+                .parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(accessToken)
+                .getBody()
+                .get("username")
+                .toString();
+
+        PrincipalUser principalUser = (PrincipalUser) principalDetailsService.loadUserByUsername(username);
+        // 유저, 서명(생략가능), 권한을 가진 authentication 객체 생성
+        authentication = new UsernamePasswordAuthenticationToken(principalUser, null, principalUser.getAuthorities());
+        return authentication;
     }
 
 }
