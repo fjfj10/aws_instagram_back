e9a8da21c7abe684d017a60d63243875b18c3f04
fjfj10 <kimmisuk2218@gmail.com>
2023-10-04 15:24:28 +0900

    feat: 회원가입시 이메일과 전화번호를 구분하여 DB에 저장하는 기능 추가

--- src/main/java/com/toyproject/instagram/service/UserService.java
+++ src/main/java/com/toyproject/instagram/service/UserService.java
@@ -2,6 +2,7 @@ package com.toyproject.instagram.service;
 
 import com.toyproject.instagram.dto.SigninReqDto;
 import com.toyproject.instagram.dto.SignupReqDto;
+import com.toyproject.instagram.entity.User;
 import com.toyproject.instagram.exception.JwtException;
 import com.toyproject.instagram.repository.UserMapper;
 import com.toyproject.instagram.security.JwtTokenProvider;
@@ -11,6 +12,9 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 @RequiredArgsConstructor
@@ -22,8 +26,26 @@ public class UserService {
     private final JwtTokenProvider jwtTokenProvider;
 
     public void signupUser(SignupReqDto signupReqDto) {
-        Integer executeCount = userMapper.saveUser(signupReqDto.toUserEntity(passwordEncoder));
-        System.out.println(executeCount);
+        User user = signupReqDto.toUserEntity(passwordEncoder);
+
+        // 입력된 정보의 email과 phone 타입을 구분하여 DB에 저장
+        String emailPattern = "^[a-zA-Z0-9]+@[0-9a-zA-Z]+\\.[a-z]*$";
+        String phonePattern = "^[0-9]{11}+$";
+
+        Pattern emailRegex = Pattern.compile(emailPattern);
+        Pattern phoneRegex = Pattern.compile(phonePattern);
+
+        Matcher emailMatcher = emailRegex.matcher(signupReqDto.getPhoneOrEmail());
+        Matcher phoneMatcher = phoneRegex.matcher(signupReqDto.getPhoneOrEmail());
+
+        if (emailMatcher.matches()) {
+            user.setEmail(signupReqDto.getPhoneOrEmail());
+        }
+        if (phoneMatcher.matches()) {
+            user.setPhone(signupReqDto.getPhoneOrEmail());
+        }
+
+        Integer executeCount = userMapper.saveUser(user);
     }
 
     public String signinUser(SigninReqDto signinReqDto) {
